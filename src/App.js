684fb80e9ed18f373857e64e488488dd383b538f
Charan Mokara <91110932+Charan-05@users.noreply.github.com>
2023-06-20 13:18:27 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from "react";
+import Auth from "./Auth";
+import AuthContext from "./auth-context";
+ 
+const App = () => {
+  //using the state to dynamicallly pass the values to the context
+ 
+  const [authstatus, setauthstatus] = useState(false);
+  const login = () => {
+    setauthstatus(true);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <AuthContext.Provider value={{ status: authstatus, login: login }}>
+        <Auth />
+      </AuthContext.Provider>
+    </React.Fragment>
   );
-}
-
+};
 export default App;
